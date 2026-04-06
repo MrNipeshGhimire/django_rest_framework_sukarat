87ecb18ed4ac86d57b9dca90c381bd3a40d785ca
Nipesh <nipeshincbca2021@gmail.com>
2026-04-06 11:48:56 +0545

    delete part complete

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views.main_view import student_view
+from .views.main_view import student_view,student_view_detail
 
 
 urlpatterns = [
-    path('student/',student_view)
+    path('student/',student_view),
+    path('student/<int:id>/',student_view_detail)
 ]
--- myproject/myapp/views/main_view.py
+++ myproject/myapp/views/main_view.py
@@ -10,7 +10,7 @@ def student_view(request):
         serializer = StudentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'msg':"Student added successfully"},status=status.HTTP_201_CREATED)
+            return Response({'msg':"Student added successfully",'data':serializer.data},status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     
@@ -19,4 +19,17 @@ def student_view(request):
         student = Student.objects.all()
         serializer = StudentSerializer(student, many=True)
         return Response({'data':serializer.data}, status=status.HTTP_200_OK)
+
+@api_view(['PUT','DELETE','GET'])
+def student_view_detail(request,id):
+    try:
+        student = Student.objects.get(id=id)
+    except Exception as e:
+        print(e)
+        return Response({"error":"Cannot found data"})
+    
+    if request.method == 'DELETE':
+        student.delete()
+        return Response({'msg':"Student data deleted successfully"},status=status.HTTP_200_OK)
+    
 
